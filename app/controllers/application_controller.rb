242087aa7aefd1e988701fa86a4acf4b3de5b011
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-20 13:41:02 -0500

    added user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  private 
+
+  def record_invalid(invalid)
+      render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity 
+  end
+
+  def record_not_found(error)
+      render json: {error: error.msg}, status: :unprocessable_entity
+  end
+
+  def current_user 
+      User.find_by(id: session[:user_id])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id # this is the piece that logs a user in and keeps track of users info in subsequent requests.
+            render json: user, status: :ok
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        if current_user
+          render json: current_user, status: :ok
+        else
+          render json: "No current session stored", status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation)
+    end
+end
