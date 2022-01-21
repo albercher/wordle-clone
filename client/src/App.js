20ba4a054ca874c9875b66ce84a641622a6047b7
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-21 12:39:59 -0500

    Added game container with route

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import GameContainer from "./components/GameContainer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<GameContainer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
---
+++ client/src/components/GameContainer.js
@@ -0,0 +1,12 @@
+
+function GameContainer(){
+    return (
+        <div>
+            test
+            {/* import grid here */}
+            {/* import keys here */}
+        </div>
+    );
+};
+
+export default GameContainer;
