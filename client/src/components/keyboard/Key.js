116b57f72969df1f4534c706b603f72026f62596
jan <janek.grochulski@gmail.com>
2022-01-25 13:02:34 -0600

    acheieved keyboard color coding functionality, grid duplicate bug remains for next commit

--- client/src/components/keyboard/Key.js
+++ client/src/components/keyboard/Key.js
@@ -7,7 +7,10 @@ function Key({ value, onClick, classn = "one", cs }) {
   // }
 
   if (cs && Object.keys(cs).includes(value)) {
-    if (cs[value] === 1) {
+    if (cs[value] === 2) {
+      classn = "one_green";
+    }
+    else if (cs[value] === 1) {
       classn = "one_yellow";
     }
     else {
--- client/src/functions/charStats.js
+++ client/src/functions/charStats.js
@@ -1,8 +1,11 @@
 import { solution } from "../constants/Solution";
 
 export const charStats = function(guesses) {
+
   let charStats = {};
   let uniqueChars = "";
+  let sol = solution.toLowerCase();
+
   if (guesses.length > 1) {
     const uniqueSet = new Set(guesses.join('').split(""));
     uniqueChars = [...uniqueSet].join('');
@@ -14,13 +17,50 @@ export const charStats = function(guesses) {
     console.log("uni: " + uniqueChars);
   }
 
-  if (uniqueChars) {
-    for (let i = 0; i < uniqueChars.length; i++) {
-      if (solution.toLowerCase().includes(uniqueChars[i])) {
-        charStats[uniqueChars[i]] = 1;
-      }
-      else {
-        charStats[uniqueChars[i]] = 0;
+  // if (uniqueChars) {
+  //   for (let i = 0; i < uniqueChars.length; i++) {
+  //     if (solution.toLowerCase().includes(uniqueChars[i])) {
+  //       console.log("guesses[guesses.length - 1][i]: " + guesses[guesses.length - 1][i])
+  //       console.log("solution[i]: " + solution[i])
+  //       console.log("guesses: ")
+  //       console.log(guesses)
+  //       if (guesses[guesses.length - 1][i] === solution[i].toLowerCase()) {
+  //         charStats[uniqueChars[i]] = 2;
+  //       }
+  //       else {
+  //       charStats[uniqueChars[i]] = 1;
+  //       }
+  //     }
+  //     else {
+  //       charStats[uniqueChars[i]] = 0;
+  //     }
+  //   }
+  // }
+
+  if (uniqueChars) {  // if uniqueChars exists, i.e. if guess is submitted
+    for (let i = 0; i < uniqueChars.length; i++) { // loop thorugh uniqueChars str
+      for (let j = 0; j < 5; j++) { // loop through solution chars
+        if (uniqueChars[i] === sol[j]) {
+          console.log("uniqueChars[i]: " + uniqueChars[i])
+          console.log("sol[j]: " + sol[j])
+          charStats[uniqueChars[i]] = 1;
+
+          for (let k = 0; k < 5; k++) {
+            if (guesses[guesses.length - 1][k] === sol[j] && j === k) {
+              charStats[uniqueChars[i]] = 2;
+            }
+            else {
+              if (charStats[uniqueChars[i]] !== 2) {
+                charStats[uniqueChars[i]] =  1;
+              }
+            }
+          }
+        }
+        else {
+          if (charStats[uniqueChars[i]] !== 2 && (charStats[uniqueChars[i]] !== 1)) {
+            charStats[uniqueChars[i]] = 0;
+          }
+        }
       }
     }
   }
