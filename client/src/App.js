89236cd7a677375c93785a2a0494f65e70c49f17
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-25 11:56:19 -0500

    Cleaning up

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,8 @@ function App() {
       }
     });
   }, []);
-  
-  // if (user) {
-  //   return <h2>Welcome, {user.username}!</h2>;
-  // } else {
-  //   return <Login onLogin={setUser} />;
-  // }
+
   return (
-    // <div>hello, {user.username}</div>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GameContainer currentUser={user} />} />
