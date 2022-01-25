a7001e41aa1abe9000987870616e13b429f1182d
jan <janek.grochulski@gmail.com>
2022-01-25 00:20:34 -0600

    commented out one very annoying console log

--- client/src/components/keyboard/Key.js
+++ client/src/components/keyboard/Key.js
@@ -2,9 +2,9 @@ import "../../keyboard.css";
 
 function Key({ value, onClick, classn = "one", cs }) {
 
-  if (cs) {
-    console.log(Object.keys(cs))
-  }
+  // if (cs) {
+  //   console.log(Object.keys(cs))
+  // }
 
   if (cs && Object.keys(cs).includes(value)) {
     if (cs[value] === 1) {
