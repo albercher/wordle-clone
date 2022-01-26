d17f8d3cf4e7eed1faeef3f5910d8f976e3ea215
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-26 12:29:29 -0500

    handle count of wins/losses

--- app/serializers/user_score_serializer.rb
+++
@@ -1,7 +0,0 @@
-class UserScoreSerializer < ActiveModel::Serializer
-  attributes :game_count
-
-  def game_count
-    "Wins: #{self.object.count}"
-  end
-end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :user_initial
+  attributes :id, :username, :email, :user_initial, :game_count, :results_count
   has_many :scores
 
   def user_initial
     self.object.username[0,1]
+  end
+
+  def game_count
+    self.object.scores.count
+  end
+
+  def results_count
+    self.object.scores.group(:win).count
   end
 end
