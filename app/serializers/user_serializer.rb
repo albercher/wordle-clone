28ef5071ae7e904268d690b90971a5cc46854d60
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-27 13:55:42 -0500

    Removed scores from user serializer since it wasnt used in front end

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :user_initial, :game_count, :results_count
-  has_many :scores
+  # has_many :scores
 
   def user_initial
     self.object.username[0,1]
